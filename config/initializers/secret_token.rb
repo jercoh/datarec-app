bb3ef3797fac4732a65c16dd06d420e405ec75aa
jercoh <jejechaber289@msn.com>
2014-03-18 08:25:45 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatarecApp::Application.config.secret_token = '0fbb3183b4791179f73970d54b7e00e1c355d581cde21e9d429ea5c811cd264b20afbc66595afa2aebf0c712294429495210f3406bbc85ca14c0878d8dbe7c23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DatarecApp::Application.config.session_store :cookie_store, key: '_datarec-app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DatarecApp::Application.config.session_store :active_record_store
